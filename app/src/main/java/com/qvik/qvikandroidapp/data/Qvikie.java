e3cf6625cafd5bee3fd872cbf2797e541c091df7
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-19 12:30:04 +0300

    Starts implementing remote data source with firebase

--- app/src/main/java/com/qvik/qvikandroidapp/data/Qvikie.java
+++ app/src/main/java/com/qvik/qvikandroidapp/data/Qvikie.java
@@ -5,28 +5,37 @@ import android.support.annotation.Nullable;
 
 import com.google.common.base.Strings;
 import com.google.common.base.Objects;
+import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.util.UUID;
 
 /**
  * Immutable model class for a Qvikie.
  */
+@IgnoreExtraProperties
 public final class Qvikie {
 
     @NonNull
-    private final String mId;
+    private String id;
 
     @Nullable
-    private final String mName;
+    private String name;
 
     @Nullable
-    private final String mTitle;
+    private String title;
 
     @Nullable
-    private final String mDescription;
+    private String description;
 
     @Nullable
-    private final String mPhoneNumber;
+    private String phoneNumber;
+
+    @Nullable
+    private String email;
+
+    public Qvikie() {
+        // Firebase requires this empty constructor
+    }
 
     /**
      * Use this constructor to create a new Qvikie.
@@ -35,10 +44,11 @@ public final class Qvikie {
      * @param title       title of the qvikie
      * @param description description of the qvikie
      * @param phoneNumber phone number of the qvikie
+     * @param email       email of the qvikie
      */
     public Qvikie(@Nullable String name, @Nullable String title, @Nullable String description,
-                  @Nullable String phoneNumber) {
-        this(UUID.randomUUID().toString(), name, title, description, phoneNumber);
+                  @Nullable String phoneNumber, @Nullable String email) {
+        this(UUID.randomUUID().toString(), name, title, description, phoneNumber, email);
     }
 
     /**
@@ -50,58 +60,69 @@ public final class Qvikie {
      * @param id          id of the task
      */
     public Qvikie(@NonNull String id, @Nullable String name, @Nullable String title,
-                  @Nullable String description, @Nullable String phoneNumber) {
-        mId = id;
-        mName = name;
-        mTitle = title;
-        mDescription = description;
-        mPhoneNumber = phoneNumber;
+                  @Nullable String description, @Nullable String phoneNumber,
+                  @Nullable String email) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.description = description;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
     @NonNull
     public String getId() {
-        return mId;
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
     }
 
     @Nullable
     public String getName() {
-        return mName;
+        return name;
     }
 
     @Nullable
     public String getTitle() {
-        return mTitle;
+        return title;
     }
 
     @Nullable
     public String getTitleForList() {
-        if (!Strings.isNullOrEmpty(mTitle)) {
-            return mTitle;
+        if (!Strings.isNullOrEmpty(title)) {
+            return title;
         } else {
-            return mDescription;
+            return description;
         }
     }
 
     @Nullable
     public String getDescription() {
-        return mDescription;
+        return description;
     }
 
     @Nullable
     public String getPhoneNumber() {
-        return mPhoneNumber;
+        return phoneNumber;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return email;
     }
 
     public boolean isEngineer() {
-        return mTitle != null && mTitle.equals("ENGINEER");
+        return title != null && title.equals("ENGINEER");
     }
 
     public boolean isDesigner() {
-        return mTitle != null && mTitle.equals("DESIGNER");
+        return title != null && title.equals("DESIGNER");
     }
 
     public boolean isEmpty() {
-        return Strings.isNullOrEmpty(mName) && Strings.isNullOrEmpty(mTitle);
+        return Strings.isNullOrEmpty(name) && Strings.isNullOrEmpty(title);
     }
 
     @Override
@@ -109,20 +130,20 @@ public final class Qvikie {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Qvikie qvikie = (Qvikie) o;
-        return Objects.equal(mId, qvikie.mId) &&
-                Objects.equal(mName, qvikie.mName) &&
-                Objects.equal(mTitle, qvikie.mTitle) &&
-                Objects.equal(mDescription, qvikie.mDescription) &&
-                Objects.equal(mPhoneNumber, qvikie.mPhoneNumber);
+        return Objects.equal(id, qvikie.id) &&
+                Objects.equal(name, qvikie.name) &&
+                Objects.equal(title, qvikie.title) &&
+                Objects.equal(description, qvikie.description) &&
+                Objects.equal(phoneNumber, qvikie.phoneNumber);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(mId, mName, mTitle, mDescription, mPhoneNumber);
+        return Objects.hashCode(id, name, title, description, phoneNumber);
     }
 
     @Override
     public String toString() {
-        return "Qvikie " + mName + " (" + mTitle + ")";
+        return "Qvikie " + name + " (" + title + ")";
     }
 }
--- app/src/main/java/com/qvik/qvikandroidapp/data/source/remote/QvikiesRemoteDataSource.java
+++ app/src/main/java/com/qvik/qvikandroidapp/data/source/remote/QvikiesRemoteDataSource.java
@@ -1,16 +1,30 @@
 package com.qvik.qvikandroidapp.data.source.remote;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.qvik.qvikandroidapp.data.Qvikie;
 import com.qvik.qvikandroidapp.data.source.QvikiesDataSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * TODO Implementation of a remote data source (firebase?)
+ * TODO Finish firebase implementation of a remote data source
  */
 public class QvikiesRemoteDataSource implements QvikiesDataSource {
 
     private static QvikiesRemoteDataSource INSTANCE;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = database.getReference("qvikies");
+
+    public static final String TAG = "QvikiesRemoteDataSource";
 
     public static QvikiesRemoteDataSource getInstance() {
         if (INSTANCE == null) {
@@ -24,18 +38,45 @@ public class QvikiesRemoteDataSource implements QvikiesDataSource {
     }
 
     @Override
-    public void getQvikies(@NonNull LoadQvikiesCallback callback) {
+    public void getQvikies(@NonNull final LoadQvikiesCallback callback) {
+        database.getReference("qvikies").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                List<Qvikie> qvikies = new ArrayList<>();
+                for(DataSnapshot data : snapshot.getChildren()){
+                    Qvikie qvikie = data.getValue(Qvikie.class);
+                    qvikie.setId(data.getKey());
+                    qvikies.add(qvikie);
+                }
+                callback.onQvikiesLoaded(qvikies);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     @Override
-    public void getQvikie(@NonNull String qvikieId, @NonNull GetQvikieCallback callback) {
+    public void getQvikie(@NonNull final String qvikieId, @NonNull final GetQvikieCallback callback) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onQvikieLoaded((Qvikie) dataSnapshot.child(qvikieId).getValue());
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
     public void saveQvikie(@NonNull Qvikie qvikie) {
-
+        databaseReference.setValue(qvikie);
     }
 
     @Override
