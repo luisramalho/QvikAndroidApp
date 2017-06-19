13f999743da91849bdf4dcb67549e2df68bbf3c3
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-19 11:33:37 +0300

    Enables injection of production implementations for QvikiesDataSource at compile time

---
+++ app/src/prod/java/com.qvik.qvikandroidapp/Injection.java
@@ -0,0 +1,24 @@
+package com.qvik.qvikandroidapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.qvik.qvikandroidapp.data.source.QvikiesRepository;
+import com.qvik.qvikandroidapp.data.source.QvikiesDataSource;
+import com.qvik.qvikandroidapp.data.source.local.QvikiesLocalDataSource;
+import com.qvik.qvikandroidapp.data.source.remote.QvikiesRemoteDataSource;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Enables injection of production implementations for
+ * {@link QvikiesDataSource} at compile time.
+ */
+public class Injection {
+
+    public static QvikiesRepository provideQvikiesRepository(@NonNull Context context) {
+        checkNotNull(context);
+        return QvikiesRepository.getInstance(QvikiesRemoteDataSource.getInstance(),
+                QvikiesLocalDataSource.getInstance(context.getApplicationContext()));
+    }
+}
