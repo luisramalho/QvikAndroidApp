9227ccf666add09fe3c25d0879ce57a53668c165
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-19 11:26:56 +0300

    Adds Application class

---
+++ app/src/main/java/com/qvik/qvikandroidapp/App.java
@@ -0,0 +1,13 @@
+package com.qvik.qvikandroidapp;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private static final String TAG = "App";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
