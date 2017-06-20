76f0474929985ee16900c773833998519578345a
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-20 17:09:15 +0300

    Begins the implementation of the local data source with realm

--- app/src/main/java/com/qvik/qvikandroidapp/data/source/local/QvikiesLocalDataSource.java
+++ app/src/main/java/com/qvik/qvikandroidapp/data/source/local/QvikiesLocalDataSource.java
@@ -6,6 +6,10 @@ import android.support.annotation.NonNull;
 import com.qvik.qvikandroidapp.data.Qvikie;
 import com.qvik.qvikandroidapp.data.source.QvikiesDataSource;
 
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
@@ -15,7 +19,7 @@ public class QvikiesLocalDataSource implements QvikiesDataSource {
 
     private static QvikiesLocalDataSource INSTANCE;
 
-    private QvikiesDbHelper mDbHelper;
+    private Realm realm;
 
     public static QvikiesLocalDataSource getInstance(@NonNull Context context) {
         if (INSTANCE == null) {
@@ -27,37 +31,48 @@ public class QvikiesLocalDataSource implements QvikiesDataSource {
     // Prevents instantiation
     private QvikiesLocalDataSource(@NonNull Context context) {
         checkNotNull(context);
-        mDbHelper = new QvikiesDbHelper(context);
+        realm = Realm.getDefaultInstance();
     }
-
 
     @Override
     public void getQvikies(@NonNull LoadQvikiesCallback callback) {
-
+        RealmQuery<Qvikie> query = realm.where(Qvikie.class);
+        callback.onQvikiesLoaded(query.findAll());
     }
 
     @Override
     public void getQvikie(@NonNull String qvikieId, @NonNull GetQvikieCallback callback) {
-
+        Qvikie qvikie = realm.where(Qvikie.class).equalTo("id", qvikieId).findFirst();
+        callback.onQvikieLoaded(qvikie);
     }
 
+    /**
+     * Persists qvikie in a transaction.
+     *
+     * @param qvikie the qvikie to be persisted.
+     */
     @Override
     public void saveQvikie(@NonNull Qvikie qvikie) {
-
+        realm.beginTransaction();
+        realm.copyToRealm(qvikie);
+        realm.commitTransaction();
     }
 
     @Override
     public void refreshQvikies() {
-
+        // Not required because the {@link QvikiesRepository} handles the logic of refreshing the
+        // qvikies from all the available data sources.
     }
 
     @Override
     public void deleteAllQvikies() {
-
+        RealmResults<Qvikie> qvikies = realm.where(Qvikie.class).findAll();
+        qvikies.deleteAllFromRealm();
     }
 
     @Override
     public void deleteQvikie(@NonNull String qvikieId) {
-
+        Qvikie qvikie = realm.where(Qvikie.class).equalTo("id", qvikieId).findFirst();
+        qvikie.deleteFromRealm();
     }
 }
