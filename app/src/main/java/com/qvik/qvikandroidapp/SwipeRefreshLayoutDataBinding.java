8347748f59402c69ffd94f092dd970bd16be52ec
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-22 17:00:49 +0300

    Implements refresh layout on qvikies list

---
+++ app/src/main/java/com/qvik/qvikandroidapp/SwipeRefreshLayoutDataBinding.java
@@ -0,0 +1,25 @@
+package com.qvik.qvikandroidapp;
+
+import android.databinding.BindingAdapter;
+import android.support.v4.widget.SwipeRefreshLayout;
+
+import com.qvik.qvikandroidapp.qvikies.QvikiesViewModel;
+
+public class SwipeRefreshLayoutDataBinding {
+
+    /**
+     * Reloads the data when the pull-to-refresh is triggered.
+     * <p>
+     * Creates the {@code android:onRefresh} for a {@link SwipeRefreshLayout}.
+     */
+    @BindingAdapter("android:onRefresh")
+    public static void setSwipeRefreshLayoutOnRefreshListener(ScrollChildSwipeRefreshLayout view,
+                                                              final QvikiesViewModel viewModel) {
+        view.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                viewModel.loadQvikies(true);
+            }
+        });
+    }
+}
