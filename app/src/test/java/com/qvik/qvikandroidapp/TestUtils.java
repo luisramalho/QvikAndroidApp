dd890c2452bd83f210d137209c12b90a9b8ff2d2
Luis Ramalho <luis.ramalho@qvik.fi>
2017-06-27 16:30:36 +0300

    Adds a test util

---
+++ app/src/test/java/com/qvik/qvikandroidapp/TestUtils.java
@@ -0,0 +1,27 @@
+package com.qvik.qvikandroidapp;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LifecycleRegistry;
+
+
+public class TestUtils {
+    public static final LifecycleOwner TEST_OBSERVER = new LifecycleOwner() {
+
+        private LifecycleRegistry registry = init();
+
+        // Creates a LifecycleRegistry in RESUMED state.
+        private LifecycleRegistry init() {
+            LifecycleRegistry registry = new LifecycleRegistry(this);
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE);
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_START);
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME);
+            return registry;
+        }
+
+        @Override
+        public Lifecycle getLifecycle() {
+            return registry;
+        }
+    };
+}
