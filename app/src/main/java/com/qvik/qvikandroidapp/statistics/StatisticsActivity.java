b4ffb9489ef39600a0ec3ff386e0d36dadf541fb
Luis Ramalho <luis.ramalho@qvik.fi>
2017-07-03 17:03:12 +0300

    Implements the Statistics activity

---
+++ app/src/main/java/com/qvik/qvikandroidapp/statistics/StatisticsActivity.java
@@ -0,0 +1,117 @@
+package com.qvik.qvikandroidapp.statistics;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.qvik.qvikandroidapp.R;
+import com.qvik.qvikandroidapp.ViewModelFactory;
+import com.qvik.qvikandroidapp.qvikies.QvikiesActivity;
+import com.qvik.qvikandroidapp.util.ActivityUtils;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.statistics_act);
+
+        setupToolbar();
+
+        setupNavigationDrawer();
+
+        findOrCreateViewFragment();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.setTitle(R.string.statistics_title);
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+            navigationView.getMenu().getItem(1).setChecked(true);
+        }
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem
+                                                                    menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_menu_item:
+                                Intent intent = new Intent(
+                                        StatisticsActivity.this,
+                                        QvikiesActivity.class
+                                );
+                                startActivity(intent);
+                                break;
+                            case R.id.statistics_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @NonNull
+    private StatisticsFragment findOrCreateViewFragment() {
+        StatisticsFragment statisticsFragment = (StatisticsFragment)
+                getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (statisticsFragment == null) {
+            statisticsFragment = StatisticsFragment.newInstance();
+            ActivityUtils.replaceFragmentInActivity(getSupportFragmentManager(),
+                    statisticsFragment, R.id.contentFrame);
+        }
+        return statisticsFragment;
+    }
+
+    public static StatisticsViewModel obtainViewModel(FragmentActivity activity) {
+        // Use a Factory to inject dependencies into the ViewModel
+        ViewModelFactory factory = ViewModelFactory.getInstance(
+                activity.getApplication());
+
+        return ViewModelProviders.of(activity, factory).get(
+                StatisticsViewModel.class);
+    }
+}
