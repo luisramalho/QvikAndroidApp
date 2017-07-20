d638ee7018e782964bbbf76a1f1cfbb821ecdf19
Luís Ramalho <luis.ramalho@qvik.fi>
2017-07-20 12:47:19 +0300

    Implements the room database class

---
+++ app/src/main/java/com/qvik/qvikandroidapp/data/source/local/QvikDatabase.java
@@ -0,0 +1,33 @@
+package com.qvik.qvikandroidapp.data.source.local;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.qvik.qvikandroidapp.data.Qvikie;
+import com.qvik.qvikandroidapp.data.QvikiesDao;
+
+/**
+ * The Room Database that contains the Task table.
+ */
+@Database(entities = {Qvikie.class}, version = 1)
+public abstract class QvikDatabase extends RoomDatabase {
+
+    private static QvikDatabase INSTANCE;
+
+    public abstract QvikiesDao qvikiesDao();
+
+    private static final Object lock = new Object();
+
+    public static QvikDatabase getInstance(Context context) {
+        synchronized (lock) {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        QvikDatabase.class, "Qvik.db")
+                        .build();
+            }
+            return INSTANCE;
+        }
+    }
+}
