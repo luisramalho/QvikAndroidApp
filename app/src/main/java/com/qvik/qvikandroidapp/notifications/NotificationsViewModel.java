1781d84156520cef07c3724250d45c0236ca7b36
Luís Ramalho <luis.ramalho@qvik.fi>
2017-07-31 16:54:57 +0300

    Implements the NotificationsViewModel

--- app/src/main/java/com/qvik/qvikandroidapp/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/qvik/qvikandroidapp/notifications/NotificationsViewModel.java
@@ -3,37 +3,43 @@ package com.qvik.qvikandroidapp.notifications;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.content.Context;
+import android.databinding.ObservableArrayList;
 import android.databinding.ObservableBoolean;
+import android.databinding.ObservableList;
 
-import com.qvik.qvikandroidapp.data.source.QvikiesDataSource;
-import com.qvik.qvikandroidapp.data.source.QvikiesRepository;
+import com.qvik.qvikandroidapp.SingleLiveEvent;
+import com.qvik.qvikandroidapp.data.Notification;
+import com.qvik.qvikandroidapp.data.source.NotificationsDataSource;
+import com.qvik.qvikandroidapp.data.source.NotificationsDataSource.LoadNotificationsCallback;
+import com.qvik.qvikandroidapp.data.source.NotificationsRepository;
+
+import java.util.List;
 
 public class NotificationsViewModel extends AndroidViewModel {
 
+    public final ObservableList<Notification> items = new ObservableArrayList<>();
+
     public final ObservableBoolean dataLoading = new ObservableBoolean(false);
+
+    public final ObservableBoolean empty = new ObservableBoolean(false);
+
+    private final ObservableBoolean isDataLoadingError = new ObservableBoolean(false);
+
+    private SingleLiveEvent<String> openNotificationEvent = new SingleLiveEvent<>();
 
     private Context context;
 
-    private final QvikiesRepository qvikiesRepository;
+    private final NotificationsRepository notificationsRepository;
 
-    public NotificationsViewModel(Application context, QvikiesRepository
-            qvikiesRepository) {
+    public NotificationsViewModel(Application context, NotificationsRepository
+            notificationsRepository) {
         super(context);
         this.context = context;
-        this.qvikiesRepository = qvikiesRepository;
+        this.notificationsRepository = notificationsRepository;
     }
 
     public void start() {
-        loadNotifications();
-    }
-
-    private void loadNotifications() {
-        dataLoading.set(true);
-
-        // TODO Get the notifications
-        // qvikiesRepository.getNotifications(...);
-
-        dataLoading.set(false);
+        loadNotifications(false);
     }
 
     public void loadNotifications(boolean forceUpdate) {
@@ -41,10 +47,38 @@ public class NotificationsViewModel extends AndroidViewModel {
     }
 
     /**
-     * @param forceUpdate   Pass in true to refresh the data in the {@link QvikiesDataSource}
+     * @param forceUpdate   Pass in true to refresh the data in the {@link NotificationsDataSource}
      * @param showLoadingUI Pass in true to display a loading icon in the UI
      */
-    private void loadNotifications(boolean forceUpdate, boolean showLoadingUI) {
-        // TODO implement
+    private void loadNotifications(boolean forceUpdate, final boolean showLoadingUI) {
+        if (showLoadingUI) {
+            dataLoading.set(true);
+        }
+        if (forceUpdate) {
+            notificationsRepository.refreshNotifications();
+        }
+
+        notificationsRepository.getNotifications(new LoadNotificationsCallback() {
+            @Override
+            public void onNotificationsLoaded(List<Notification> notifications) {
+                if (showLoadingUI) {
+                    dataLoading.set(false);
+                }
+                isDataLoadingError.set(false);
+
+                items.clear();
+                items.addAll(notifications);
+                empty.set(items.isEmpty());
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                isDataLoadingError.set(true);
+            }
+        });
+    }
+
+    SingleLiveEvent<String> getOpenNotificationEvent() {
+        return openNotificationEvent;
     }
 }
