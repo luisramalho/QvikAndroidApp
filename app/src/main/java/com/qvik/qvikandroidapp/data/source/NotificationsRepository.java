8dc36be753636483f315aa10a00ca6c4af685add
Luís Ramalho <luis.ramalho@qvik.fi>
2017-08-01 16:43:00 +0300

    Implements most methods of notifications repository

--- app/src/main/java/com/qvik/qvikandroidapp/data/source/NotificationsRepository.java
+++ app/src/main/java/com/qvik/qvikandroidapp/data/source/NotificationsRepository.java
@@ -1,6 +1,7 @@
 package com.qvik.qvikandroidapp.data.source;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.qvik.qvikandroidapp.data.Notification;
 import com.qvik.qvikandroidapp.util.EspressoIdlingResource;
@@ -95,6 +96,7 @@ public class NotificationsRepository implements NotificationsDataSource {
                 @Override
                 public void onNotificationsLoaded(List<Notification> notifications) {
                     refreshCache(notifications);
+                    refreshLocalDataSource(notifications);
 
                     EspressoIdlingResource.decrement(); // Set app as idle.
 
@@ -110,20 +112,116 @@ public class NotificationsRepository implements NotificationsDataSource {
         }
     }
 
+    /**
+     * Gets notifications from the local data source (realm) unless the table is
+     * new or empty. In that case it uses the network data source.
+     * This is done to simplify the sample.
+     * <p>
+     * Note: {@link GetNotificationCallback#onDataNotAvailable()} is fired if
+     * both data sources fail to get the data.
+     */
     @Override
-    public void getNotification(@NonNull String notificationId,
-                                @NonNull GetNotificationCallback callback) {
-        // TODO
+    public void getNotification(@NonNull final String notificationId,
+                                @NonNull final GetNotificationCallback callback) {
+        //noinspection ResultOfMethodCallIgnored
+        checkNotNull(notificationId, "notificationId == null");
+        //noinspection ResultOfMethodCallIgnored
+        checkNotNull(callback, "callback == null");
+
+        Notification cachedNotification = getNotificationWithId(notificationId);
+
+        // Respond immediately with cache if available
+        if (cachedNotification != null) {
+            callback.onNotificationLoaded(cachedNotification);
+            return;
+        }
+
+        EspressoIdlingResource.increment(); // App is busy until further notice
+
+        // Load from server/persisted if needed.
+
+        // Is the notification in the local data source? If not, query the network.
+        notificationsLocalDataSource.getNotification(notificationId, new GetNotificationCallback() {
+            @Override
+            public void onNotificationLoaded(Notification notification) {
+                if (notification == null) {
+                    onDataNotAvailable();
+                    return;
+                }
+
+                // Do in memory cache update to keep the app UI up to date
+                if (cachedNotifications == null) {
+                    cachedNotifications = new LinkedHashMap<>();
+                }
+                cachedNotifications.put(notification.getId(), notification);
+
+                EspressoIdlingResource.decrement(); // Set app as idle.
+
+                callback.onNotificationLoaded(notification);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                notificationsRemoteDataSource.getNotification(notificationId, new GetNotificationCallback() {
+                    @Override
+                    public void onNotificationLoaded(Notification notification) {
+                        // Do in memory cache update to keep the app UI up to date
+                        if (cachedNotifications == null) {
+                            cachedNotifications = new LinkedHashMap<>();
+                        }
+                        cachedNotifications.put(notification.getId(), notification);
+
+                        EspressoIdlingResource.decrement(); // Set app as idle.
+
+                        callback.onNotificationLoaded(notification);
+                    }
+
+                    @Override
+                    public void onDataNotAvailable() {
+                        EspressoIdlingResource.decrement(); // Set app as idle.
+
+                        callback.onDataNotAvailable();
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public void saveNotification(@NonNull Notification notification) {
-        // TODO
+        //noinspection ResultOfMethodCallIgnored
+        checkNotNull(notification, "notification == null");
+
+        notificationsRemoteDataSource.saveNotification(notification);
+        notificationsLocalDataSource.saveNotification(notification);
+
+        // Do in memory cache update to keep the app UI up to date
+        if (cachedNotifications == null) {
+            cachedNotifications = new LinkedHashMap<>();
+        }
+        cachedNotifications.put(notification.getId(), notification);
     }
 
     @Override
     public void deleteNotification(@NonNull String notificationId) {
-        // TODO
+        //noinspection ResultOfMethodCallIgnored
+        checkNotNull(notificationId, "notificationId == null");
+
+        notificationsRemoteDataSource.deleteNotification(notificationId);
+        notificationsLocalDataSource.deleteNotification(notificationId);
+
+        cachedNotifications.remove(notificationId);
+    }
+
+    @Override
+    public void deleteAllNotifications() {
+        notificationsRemoteDataSource.deleteAllNotifications();
+        notificationsLocalDataSource.deleteAllNotifications();
+
+        if (cachedNotifications== null) {
+            cachedNotifications = new LinkedHashMap<>();
+        }
+        cachedNotifications.clear();
     }
 
     @Override
@@ -131,12 +229,13 @@ public class NotificationsRepository implements NotificationsDataSource {
         cacheIsDirty = true;
     }
 
-    private void getNotificationsFromRemoteDataSource(@NonNull final LoadNotificationsCallback callback) {
+    private void getNotificationsFromRemoteDataSource(
+            @NonNull final LoadNotificationsCallback callback) {
         notificationsRemoteDataSource.getNotifications(new LoadNotificationsCallback() {
             @Override
             public void onNotificationsLoaded(List<Notification> notifications) {
                 refreshCache(notifications);
-//                refreshLocalDataSource(notifications);
+                refreshLocalDataSource(notifications);
 
                 EspressoIdlingResource.decrement(); // Set app as idle.
 
@@ -161,5 +260,24 @@ public class NotificationsRepository implements NotificationsDataSource {
             cachedNotifications.put(notification.getId(), notification);
         }
         cacheIsDirty = false;
+    }
+
+    private void refreshLocalDataSource(List<Notification> notifications) {
+        notificationsLocalDataSource.deleteAllNotifications();
+        for (Notification notification : notifications) {
+            notificationsLocalDataSource.saveNotification(notification);
+        }
+    }
+
+    @Nullable
+    private Notification getNotificationWithId(@NonNull String notificationId) {
+        //noinspection ResultOfMethodCallIgnored
+        checkNotNull(notificationId, "notificationId == null");
+
+        if (cachedNotifications == null || cachedNotifications.isEmpty()) {
+            return null;
+        } else {
+            return cachedNotifications.get(notificationId);
+        }
     }
 }
